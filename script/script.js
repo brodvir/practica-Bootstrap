45f678b424e59b86d7ec1e369c680ce42c0034c9
[barbaraarv] <[barbaraarv98@gmail.com]>
2023-06-20 01:20:28 +0200

    initial commit

---
+++ script/script.js
@@ -0,0 +1,83 @@
+
+function validar(event){
+    const nombre = document.getElementById("nombre");
+    const email = document.getElementById("email")
+    const email_verification = document.getElementById("email_verification")
+    const edad = document.getElementById("edad")
+    const password = document.getElementById("password")
+    const provincia = document.getElementById("provincia")
+    const terminos = document.getElementById("terminos")
+    let correcto =true
+    
+    if (nombre.value.length==0) {
+        event.preventDefault();
+        correcto=false;
+        nombre.style.borderColor="red";
+        document.getElementById("errorNombre").style.visibility="visible";
+        event.preventDefault();
+    }
+
+    if (email.validity.typeMismatch) {
+        correcto=false;
+        nombre.style.borderColor="red";
+        document.getElementById("errorEmail").style.visibility="visible";
+      }
+    if (email_verification!= email) {
+        correcto=false;
+        nombre.style.borderColor="red";
+        document.getElementById("errorSegundoEmail").style.visibility="visible";
+    }
+
+    if (isNaN(edad.value) || edad.value<18 || edad.value>120 ) {
+        correcto=false;
+        edad.style.borderColor="red";
+        document.getElementById("errorEdad").style.visibility="visible";
+    }
+
+    let mayus = 0
+    let min = 0
+    let num = 0
+    let signo = 0
+    for(var i = 0; i< password.value.length; i++){
+        if(password.value[i]>= A && password.value[i]<= Z){
+            mayus=1
+        }
+        if(password.value[i]>= a && password.value[i]<= z){
+            min=1
+        }
+        if(password.value[i]>= 0 && password.value[i]<= 9){
+            num=1
+        }
+        if(password.value[i]== "." || password.value[i]== "," || password.value[0]== ":" ){
+            signo=1
+        }
+    }
+
+    if (mayus < 1 || min < 1 || num < 1 || signo < 1 ) {
+        correcto=false;
+        anyo.style.borderColor="red";
+        document.getElementById("errorPassword").style.visibility="visible";
+    }
+
+    if (terminos.value == false) {
+        correcto=false;
+        terminos.style.borderColor="red";
+        document.getElementById("errorTerminos").style.visibility="visible";
+    }
+
+    if (!correcto){
+        document.getElementById("mensajeError").innerHTML="datos erróneos en el formulario";
+     }
+
+     return correcto
+
+
+}
+
+function quitarError(nodo,id){
+    nodo.style.borderColor="lightgray";
+    document.getElementById(id).style.visibility="hidden";
+}
+
+
+
